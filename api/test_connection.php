270a4068efc4b32ef6803db895ec9a3bb0e22e83
ErenBal <erenbal.academic@gmail.com>
2025-12-17 17:20:06 +0300

    Fix require statement in test_connection.php to reference the correct CRUD file

--- api/test_connection.php
+++ api/test_connection.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-require_once __DIR__ . '/../includes/crud.php';
+require_once __DIR__ . '/../includes/cruds.php';
 
 /**
  * Veritabanı bağlantısını ve CRUD işlemlerini test eden dosya
